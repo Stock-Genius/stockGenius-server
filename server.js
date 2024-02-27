84a4aff32cac386d86eec3c76f52a01c946119d6
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-02-28 01:06:35 +0500

    test

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use("/api/upload" , uploadRoutes);
 const __dirname=path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+console.log( express.static(path.join(__dirname, '/uploads')));
+
 app.get('/', (req, res) => {
     res.send('Api is running...')
 })
