01ea16e71b61b7646f82e1e208763ade84021c47
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-02-28 01:36:51 +0500

    test

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -5,15 +5,15 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'uploads/')
+        // cb(null, 'uploads/')
+        cb(null, path.join(__dirname, '/uploads/'));
+
     },
     filename(req, file, cb) {
-        // cb(
-        //     null,
-        //     `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-        // )
-        const name = Date.now() + file.originalname;
-        cb(null, name);
+        cb(
+            null,
+            `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+        )
     },
 })
 function checkFileType(file, cb) {
--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.use("/api/upload" , uploadRoutes);
 
 
 const __dirname=path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(__dirname));
+
 
 app.get('/', (req, res) => {
     res.send('Api is running...')
