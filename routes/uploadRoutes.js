b85dc715e8cbe73444737f891925ad8e05a00d3a
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-02-28 01:46:25 +0500

    test

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from 'express'
 import multer from 'multer'
 const router = express.Router()
+const __dirname=path.resolve()
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
