a04c0ab4996c1cbbd91b17a5ec38dddda9dc1b2c
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-02-28 15:41:27 +0500

    add cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import cors from "cors";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
 dotenv.config();
 connectDB();
 
